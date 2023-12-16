d1281ee1818906931fefec608669bbb35801af22
Vimal Mollyn <ms123vimal@gmail.com>
2023-12-16 17:55:27 -0500

    Init commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Alscribe
+![Main page of the tool](app.png)
+
+Minimal, hackable, GPT powered text editor. Really really really barebones. Tab to complete. 
+
+### 1. Add your api key to `api_key.txt`
+Not very safe, but good enough for running locally.
+### 2. Run in the root dir (python3)
+```
+python -m http.server
+```
+
+
---
+++ script.js
@@ -0,0 +1,147 @@
+const textInput = document.getElementById('textInput');
+const autocompleteDiv = document.getElementById('autocomplete');
+textInput.focus();
+
+textInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Tab') {
+        e.preventDefault();
+        textInput.innerHTML += autocompleteDiv.innerText;
+        autocompleteDiv.innerText = '';
+        autocompleteDiv.style.display = 'none';
+
+        // Move cursor to end of text
+        const range = document.createRange();
+        const selection = window.getSelection();
+        range.setStart(textInput, textInput.childNodes.length);
+        range.collapse(true);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+    }
+});
+
+// load the text from local storage if it exists
+var text = localStorage.getItem('text');
+if (text) {
+    textInput.innerHTML = text;
+    console.log('Loaded from local storage');
+    
+    // Move cursor to end of text
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.setStart(textInput, textInput.childNodes.length);
+    range.collapse(true);
+    selection.removeAllRanges();
+    selection.addRange(range);
+}
+
+// keep saving it to local storage everytime there is a change
+textInput.addEventListener('input', function() {
+    var text = textInput.innerHTML;
+    console.log(`${text}`);
+    localStorage.setItem('text', text);
+});
+
+// const apiKey = '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'; // Replace with your actual API key
+// get the api key from a local file 
+var apiKey = '';
+fetch("api_key.txt")
+  .then((res) => res.text())
+  .then((text) => {
+    console.log(text);
+    apiKey = text.trim();
+    // do something with "text"
+   })
+  .catch((e) => console.error(e));
+
+const apiUrl = 'https://api.openai.com/v1/chat/completions'; // Adjust the engine if needed
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${apiKey}`
+};
+
+// everytime the user stops typing, send a request to the server
+var userIsTyping = false;
+
+async function sendGPTRequest () {
+    console.log('Done typing.');
+    console.log('Sending to GPT-3...');
+    const data = {
+      model: 'gpt-4-1106-preview',
+      messages: [
+          {"role": "system", "content": "You are a writing assistant. Your task is to finish the sentence. You might need to add a space at the beginning."},
+          {"role": "user", "content": textInput.innerText}
+      ],
+      temperature: 0.7,
+      max_tokens: 10 // Adjust as needed
+    };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${apiKey}` },
+      body: JSON.stringify(data)
+    };
+
+    console.log(requestOptions);
+    console.log(JSON.stringify(data));
+
+    fetch(apiUrl, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        let gpt_response = data.choices[0].message.content;
+        console.log(gpt_response);
+        autocompleteDiv.style.display = 'block';
+        autocompleteDiv.innerHTML = gpt_response;
+
+        // render the text in light gray and accept the text if the user presses tab 
+        // document.getElementById('textInput').value += gpt_response;
+
+        //
+        // add the response to the text area and keep the cursor in the same place
+        // var textarea = document.getElementById('textInput');
+        // var val = textarea.value;
+        // var start = textarea.selectionStart;
+        // var end = textarea.selectionEnd;
+        // textarea.value = val.substring(0, start) + gpt_response + val.substring(end);
+        // textarea.selectionStart = textarea.selectionEnd = start + gpt_response.length;
+        // textarea.focus();
+      })
+      .catch(error => {
+        // Handle any errors here
+        console.error('Error:', error);
+      });
+}
+
+// everytime the user clicks a button, send a request to the server 
+document.getElementById('sendtogpt').addEventListener('click', sendGPTRequest);
+
+let typingTimer;
+textInput.addEventListener('input', () => {
+    console.log(`${autocompleteDiv.innerHTML}`);
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(() => {
+        updateAutocompletePosition();
+        // Call your backend to get GPT suggestions
+        // Display suggestions in autocompleteDiv
+    }, 10); // 500 ms delay after user stops typing
+    gptTimer = setTimeout(() => {
+        sendGPTRequest();
+        // Display suggestions in autocompleteDiv
+    }, 200); // 500 ms delay after user stops typing
+});
+
+function updateAutocompletePosition() {
+    const range = window.getSelection().getRangeAt(0);
+    const rect = range.getClientRects()[0];
+    const scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    console.log(rect, range)
+    if (rect) {
+        autocompleteDiv.innerHTML = '';
+        autocompleteDiv.style.display = 'block';
+        autocompleteDiv.style.left = `${rect.left + scrollLeft}px`;
+        autocompleteDiv.style.top = `${rect.top + scrollTop}px`;
+    }
+}
+
